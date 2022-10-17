2a6db1f80148ee731ad8e5826bbd61d02e24298d
luojx <luojiaxing@xiongmaitech.com>
2022-10-17 10:26:59 +0800

    在线监听客户端restful api

--- src/main/java/com/sobev/longpolling/server/ConfigServer.java
+++ src/main/java/com/sobev/longpolling/server/ConfigServer.java
@@ -1,9 +1,9 @@
 package com.sobev.longpolling.server;
 
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.*;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.Data;
 import lombok.SneakyThrows;
@@ -26,72 +26,82 @@ import java.util.concurrent.TimeUnit;
 
 
 /**
-* @author luojx
-* @date 2021/9/6 14:08
-* https://www.fatalerrors.org/a/understanding-long-polling-how-configuration-center-implements-push.html
-*/
+ * @author luojx
+ * @date 2021/9/6 14:08
+ * https://www.fatalerrors.org/a/understanding-long-polling-how-configuration-center-implements-push.html
+ */
 @Slf4j
 @SpringBootApplication
 @RestController
 public class ConfigServer {
-  @Data
-  private static class AsyncTask {
-    // The context of the long polling request, including the request and response body
-    private AsyncContext asyncContext;
-    // Timeout flag
-    private boolean timeout;
+    @Data
+    private static class AsyncTask {
+        // The context of the long polling request, including the request and response body
+        private AsyncContext asyncContext;
+        // Timeout flag
+        private boolean timeout;
 
-    public AsyncTask(AsyncContext asyncContext, boolean timeout) {
-      this.asyncContext = asyncContext;
-      this.timeout = timeout;
+        public AsyncTask(AsyncContext asyncContext, boolean timeout) {
+            this.asyncContext = asyncContext;
+            this.timeout = timeout;
+        }
     }
-  }
 
-  // guava Multiple values provided Map，One key Can correspond to multiple value
-  private Multimap<String, AsyncTask> dataIdContext = Multimaps.synchronizedSetMultimap(HashMultimap.create());
+    // guava Multiple values provided Map，One key Can correspond to multiple value
+    private Multimap<String, AsyncTask> dataIdContext = Multimaps.synchronizedSetMultimap(HashMultimap.create());
 
-  private ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("longPolling-timeout-checker-%d")
-          .build();
-  private ScheduledExecutorService timeoutChecker = new ScheduledThreadPoolExecutor(1, threadFactory);
+    private ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("longPolling-timeout-checker-%d")
+            .build();
+    private ScheduledExecutorService timeoutChecker = new ScheduledThreadPoolExecutor(1, threadFactory);
 
-  @RequestMapping("/listener")
-  public void addListener(HttpServletRequest request, HttpServletResponse response){
-    String dataId = request.getParameter("dataId");
+    private static final Cache<String, String> CLIENT_HEART_BEAT = CacheBuilder.newBuilder()
+            .expireAfterWrite(40L, TimeUnit.SECONDS)
+            .build();
 
-    // Turn on asynchronous
-    AsyncContext asyncContext = request.startAsync(request, response);
-    AsyncTask asyncTask = new AsyncTask(asyncContext,true);
+    @RequestMapping("/listener")
+    public void addListener(HttpServletRequest request, HttpServletResponse response) {
+        String dataId = request.getParameter("dataId");
+        CLIENT_HEART_BEAT.put("CLIENT_HEART_BEAT", request.getRemoteHost() + "_" + dataId);
+        // Turn on asynchronous
+        AsyncContext asyncContext = request.startAsync(request, response);
+        AsyncTask asyncTask = new AsyncTask(asyncContext, true);
 
-    // maintain dataId And asynchronous request context
-    dataIdContext.put(dataId, asyncTask);
-    timeoutChecker.schedule(() -> {
-      if(asyncTask.isTimeout()){
-        dataIdContext.remove(dataId,asyncTask);
-        response.setCharacterEncoding("UTF-8");
-        response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
-        asyncContext.complete();
-      }
-    },30000, TimeUnit.MILLISECONDS);
-  }
-
-  // Configure publishing access point
-  @PostMapping("/publishConfig")
-  @SneakyThrows
-  public String publishConfig(@RequestBody ConfigDto configDto){
-    log.info("publish configInfo dataId: [{}], configInfo: {}", configDto.getDataId(), configDto.getConfigInfo());
-    Collection<AsyncTask> asyncTasks = dataIdContext.removeAll(configDto.getDataId());
-    for(AsyncTask task:asyncTasks){
-      task.setTimeout(false);
-      HttpServletResponse response = (HttpServletResponse)task.getAsyncContext().getResponse();
-      response.setStatus(HttpServletResponse.SC_OK);
-      response.setCharacterEncoding("UTF-8");
-      response.getWriter().println(configDto.getConfigInfo());
-      task.getAsyncContext().complete();
+        // maintain dataId And asynchronous request context
+        dataIdContext.put(dataId, asyncTask);
+        timeoutChecker.schedule(() -> {
+            if (asyncTask.isTimeout()) {
+                dataIdContext.remove(dataId, asyncTask);
+                response.setCharacterEncoding("UTF-8");
+                response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+                asyncContext.complete();
+            }
+        }, 30000, TimeUnit.MILLISECONDS);
     }
-    return "success";
-  }
 
-  public static void main(String[] args) {
-    SpringApplication.run(ConfigServer.class, args);
-  }
+    // Configure publishing access point
+    @PostMapping("/publishConfig")
+    @SneakyThrows
+    public String publishConfig(@RequestBody ConfigDto configDto) {
+        log.info("publish configInfo dataId: [{}], configInfo: {}", configDto.getDataId(), configDto.getConfigInfo());
+        Collection<AsyncTask> asyncTasks = dataIdContext.removeAll(configDto.getDataId());
+        for (AsyncTask task : asyncTasks) {
+            task.setTimeout(false);
+            HttpServletResponse response = (HttpServletResponse) task.getAsyncContext().getResponse();
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().println(configDto.getConfigInfo());
+            task.getAsyncContext().complete();
+        }
+        return "success";
+    }
+
+    @PostMapping("/clients")
+    public Object connectedClients() {
+        ImmutableMap<String, String> client_heart_beat = CLIENT_HEART_BEAT.getAllPresent(ImmutableList.of("CLIENT_HEART_BEAT"));
+        return client_heart_beat;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
 }
